829b492707a936fa53c654f5d4f27b53fb82565e
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-05-05 16:22:08 +0930

    first screen

--- lib/pages/pomodoro_page.dart
+++ lib/pages/pomodoro_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:star_focus/pages/select_time_page.dart';
 import 'package:star_focus/utils/style.dart';
 import 'package:star_focus/widgets/buttons.dart';
+import 'package:star_focus/widgets/time_options.dart';
 import 'package:star_focus/widgets/timer.dart';
 
 class PomodoroPage extends StatefulWidget {
@@ -28,12 +29,13 @@ class _PomodoroPageState extends State<PomodoroPage> {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => SelectTimePage()));
+                          builder: (context) => const SelectTimePage()));
                 },
                 child: const Timer(
-                  minutes: '',
-                  seconds: '',
+                  timer: '25:00',
                 )),
+            const SizedBox(height: 20),
+            const TimeOptions(),
             const SizedBox(height: 20),
             boxButton(text: 'Start Focus', onPress: () {}),
           ],
--- lib/widgets/timer.dart
+++ lib/widgets/timer.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:star_focus/utils/style.dart';
 
 class Timer extends StatelessWidget {
-  final String minutes;
-  final String seconds;
-  const Timer({super.key, required this.minutes, required this.seconds});
+  final String timer;
+
+  const Timer({super.key, required this.timer});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,7 @@ class Timer extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          '${minutes.isNotEmpty ? minutes : '25'}:${seconds.isNotEmpty ? seconds : '00'}',
+          timer,
           style: textStyle(96, textColor),
         ),
         const SizedBox(width: 5),
